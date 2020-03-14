adfdd05a6ddbb7ca1e6b474deabb004087eab1ee
jbrotsos <j.brotsos@gmail.com>
2020-03-14 01:46:49 +0000

    get_stats.py

--- get_stats.py
+++ get_stats.py
@@ -1,24 +1,4 @@
 """
-1. get token (auth header)
-2. get team id
-3. create project with default configuration, will get project id
-4. set remote source setting to git
-5[optional]. set issue tracking system as jira by id
-6. set data retention settings by project id
-7. define SAST scan settings
-8. create new scan, will get a scan id
-9. get scan details by scan id
-10[optional]: get scan queue details by scan id
-11[optional]. get statistics results by scan id
-12. register scan report
-13. get report status by id
-14. get report by id
-15[optional]. filter report results
-
----
-1. get token (auth header)
-2. get list of projects
-3. get list of scans for each project
 
 """
 import time
@@ -31,14 +11,11 @@ from CheckmarxPythonSDK.CxRestAPISDK import TeamAPI
 from CheckmarxPythonSDK.CxRestAPISDK import ProjectsAPI
 from CheckmarxPythonSDK.CxRestAPISDK import ScansAPI
 
-
-
-
 def create_fixed_elem (prev_list, current_list, date):
     for i in prev_list:
         for j in current_list:
             found = False
-        
+
             if (i['SID'] == j['SID']):
                 found = True
                 break
@@ -103,16 +80,18 @@ def parse_xml (doc):
 
         return (vulnList, xml_results["@ScanStart"])
 
+    return [], "ERROR" 
+
 def get_project_results(file):
     scan_api = ScansAPI()
     projects_api = ProjectsAPI()
     report = []
-    current_scan_results = []
-    last_scan_results = []
 
     projects = projects_api.get_all_project_details()
 
     for project in projects:
+        current_scan_results = []
+        last_scan_results = []
 
         scans = scan_api.get_all_scans_for_project(project.project_id, "Finished")
         scans.reverse()
@@ -132,7 +111,8 @@ def get_project_results(file):
 
                     if document:
                         current_scan_results, date = parse_xml (document)
-                        create_fixed_elem(last_scan_results, current_scan_results, date)
+                        if last_scan_results:
+                            create_fixed_elem(last_scan_results, current_scan_results, date)
                         report.append(current_scan_results)
                         
                     else:
@@ -142,16 +122,16 @@ def get_project_results(file):
             else:
                 print ("[ERROR] scan report not found for " + str(scan.id))
 
+            last_scan_results = current_scan_results
 
-    file.write (json.dumps(report))
-
-    last_scan_results = current_scan_results 
-
+    return (json.dumps(report))
 
 if __name__ == "__main__":
 
     file = open("list_of_vulns.json","w")
 
-    get_project_results(file)
+    json_str = get_project_results(file)
+
+    file.write (json_str)
 
     file.close()
